f273215eb688e29de90493a91393fb51a659a79a
CondyClap <juegosfera77@gmail.com>
2024-06-06 10:53:27 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/endpoint', methods=['GET'])
+def handle_request():
+    message = request.args.get('message')
+    response = process_message(message)
+    return jsonify({'reply': response})
+
+def process_message(message):
+    if message == 'informacion_del_boton':
+        api_response = "Información recibida y procesada en el servidor Python."
+    else:
+        api_response = "Procesado: " + message
+    return api_response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
